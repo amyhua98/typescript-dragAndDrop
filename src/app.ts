826b60a0cda90951507349c7de0a1048610d67fd
Amy Hua <ahua@bigtime.net>
2024-10-09 14:23:08 -0500

    Update Project Singleton logic

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,43 @@
 //   return adjDescriptor;
 // }
 
+//Project State Management
+//singleton - design pattern ensures that a class has only one instance and provides a global point of access to that instanc - good for consistency, controlled access, thread safety
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+  private constructor() {}
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  //push all functions in a list so when it loops
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+
+  addProject(title: string, description: string, numOfPpl: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      people: numOfPpl,
+    };
+    this.projects.push(newProject);
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice());
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 //Validation logic
 interface Validatable {
   value: string | number;
@@ -62,13 +99,14 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
+  assignedProjects: any[];
 
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
       "project-list"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
+    this.assignedProjects = [];
     const importedNode = document.importNode(
       this.templateElement.content,
       true
@@ -76,8 +114,25 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
+    //list of projects, what we want to do to each project
+    projectState?.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+      this.renderProject();
+    });
+
     this.attach();
     this.renderContent();
+  }
+
+  private renderProject() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    for (const projectItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = projectItem.title;
+      listEl?.appendChild(listItem);
+    }
   }
 
   private renderContent() {
@@ -177,8 +232,8 @@ class ProjectInput {
     const userInput = this.gatherUserInput();
     if (Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
+      projectState?.addProject(title, desc, people);
       console.log(title, desc, people);
-      this.clearInputs();
     }
   }
 
